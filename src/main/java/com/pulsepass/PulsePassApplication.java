f01c9431e48b9f2d2cdab9847b18d5c0d65fd9df
Daniel Simanca <danisimanstiven@gmail.com>
2026-09-20 22:17:31 -0500

    Implementacion capa de persistencia

---
+++ src/main/java/com/pulsepass/PulsePassApplication.java
@@ -0,0 +1,11 @@
+package com.pulsepass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsePassApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PulsePassApplication.class, args);
+    }
+}
